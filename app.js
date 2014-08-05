1200eb4e6a99aec3b31f0b5c13f403ebd1dc5ff0
qixing <qixing@mogujie.com>
2014-08-05 20:27:24 +0800

    增加一点示例

--- app.js
+++ app.js
@@ -7,10 +7,53 @@ require('./route')(app)
 
 app.listen(3000)
 
-var map = {
-    "ubuntu14.04": {
-        title: "Ubuntu官方镜像",
-        desc: "Ubuntu官方镜像，apt-get update截至2014年8月3日"
+var Manager = require('port-manager');
+var manager = new Manager();
 
+manager.include(2999, 9000)
+    .claim('http1')
+    .claim('http2')
+    .claim('http2')
+    .claim('http2')
+    .claim('http2')
+    .claim('http2')
+    .claim('http2')
+;
+
+manager.on('claim', function (service) {
+    console.log(service.name, service.port);
+    // in your terminal you see two rows:
+    // http1  2000
+    // http2  2101
+});
+
+
+var map = {
+    paas: {
+        "ubuntu14.04": {
+            title: "Ubuntu 14.04",
+            desc: "Ubuntu官方镜像，apt-get update截至2014年8月3日",
+            run: "docker run -d -p $port:22 ubuntu:14.04.sshd /usr/sbin/sshd -D"
+        },
+        "centos 7": {
+            title: "CentOS 7",
+            desc: "CentOS官方镜像",
+            run: "docker run -d -p $port:22 centos:sshd /usr/sbin/sshd -D"
+        }
+    },
+    laas: {
+        "mysql": {
+            title: "MySQL 5.6.17",
+            desc: "默认密码为123",
+            run: "docker run -d -p $port:3306 -e MYSQL_ROOT_PASSWORD=123 mysql:5.6.17"
+        },
+        "mongodb": {
+            title: "MongoDB",
+            desc: "MongoDB",
+            run: "docker run -d -p $port:27017 mongo:2.6.1"
+        },
+        "redis": {
+            run: "docker run -d -p 3333:6379 redis:2.8.9"
+        }
     }
 }
--- route/home/index.js
+++ route/home/index.js
@@ -1,11 +1,12 @@
 var route = require('express').Router()
 var Docker = require('dockerode')
-var docker = new Docker({host: 'http://localhost', port: 2375});
+var docker = new Docker({host: 'http://10.0.13.56', port: 2375});
 
 route.get('/', function (req, res) {
     res.render('index')
     docker.listContainers(function (err, containers) {
-       //console.log(err, containers)
+        console.log(err, containers)
+        return;
         containers.forEach(function (containerInfo) {
             //console.log(containerInfo)
         });
