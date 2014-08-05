a4f9b7e9f7ff913181f9d4e23fb64b3dd72ccc24
qixing <qixing@mogujie.com>
2014-08-05 20:39:16 +0800

    增加一点示例

--- app.js
+++ app.js
@@ -27,33 +27,3 @@ manager.on('claim', function (service) {
     // http2  2101
 });
 
-
-var map = {
-    paas: {
-        "ubuntu14.04": {
-            title: "Ubuntu 14.04",
-            desc: "Ubuntu官方镜像，apt-get update截至2014年8月3日",
-            run: "docker run -d -p $port:22 ubuntu:14.04.sshd /usr/sbin/sshd -D"
-        },
-        "centos 7": {
-            title: "CentOS 7",
-            desc: "CentOS官方镜像",
-            run: "docker run -d -p $port:22 centos:sshd /usr/sbin/sshd -D"
-        }
-    },
-    laas: {
-        "mysql": {
-            title: "MySQL 5.6.17",
-            desc: "默认密码为123",
-            run: "docker run -d -p $port:3306 -e MYSQL_ROOT_PASSWORD=123 mysql:5.6.17"
-        },
-        "mongodb": {
-            title: "MongoDB",
-            desc: "MongoDB",
-            run: "docker run -d -p $port:27017 mongo:2.6.1"
-        },
-        "redis": {
-            run: "docker run -d -p 3333:6379 redis:2.8.9"
-        }
-    }
-}
--- route/home/index.js
+++ route/home/index.js
@@ -4,17 +4,6 @@ var docker = new Docker({host: 'http://10.0.13.56', port: 2375});
 
 route.get('/', function (req, res) {
     res.render('index')
-    docker.listContainers(function (err, containers) {
-        console.log(err, containers)
-        return;
-        containers.forEach(function (containerInfo) {
-            //console.log(containerInfo)
-        });
-    });
-
-    docker.listImages(function (err, containers) {
-        console.log(err, containers)
-    });
 })
 
 module.exports = route
